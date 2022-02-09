94b8cfc5190925c2181dd0071b50df6346eae253
Dax Levey <levey.dax@gmail.com>
2022-02-09 18:45:59 +0000

    adds method to create list of links by date

--- web_scraper.py
+++ web_scraper.py
@@ -17,6 +17,18 @@ class Scraper:
         today = datetime.datetime.today()
         return [today - datetime.timedelta(days=x) for x in range(days)]
 
+    def create_date_links(self, date_list):
+        base_url = ('https://racing.hkjc.com/racing/information/'
+                    'English/Racing/LocalResults.aspx?RaceDate=')
+        date_links = []
+        for date in date_list:
+            year = str(date.year)
+            month = str(date.month).zfill(2)
+            day = str(date.day).zfill(2)
+            link = base_url+year+'/'+month+'/'+day
+            date_links.append(link)
+        return date_links
+
     def get_runners(self):
         runners = self.driver.find_elements_by_xpath(
             '//table/tbody[@class="f_fs12"]/tr/td[3]'
@@ -33,5 +45,5 @@ if __name__ == '__main__':
     URL = ('https://racing.hkjc.com/racing/information/'
            'English/Racing/LocalResults.aspx?RaceDate=2022/02/06')
     scr = Scraper(URL)
-    print(scr.date_list(10))
+    print(scr.create_date_links(scr.date_list(10))[3])
     scr.get_runners()
