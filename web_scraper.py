d3984c00494a298011a8c365ddddc1244f84f84b
Dax Levey <levey.dax@gmail.com>
2022-02-15 18:05:50 +0000

    adds table extractor

--- web_scraper.py
+++ web_scraper.py
@@ -1,17 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import numpy as np
 import time
 import datetime
+# from uuid import uuid4
 
 
 class Scraper:
-    def __init__(self, URL):
-        self.chrome_options = Options().add_argument("--headless")
+    def __init__(self):
+        self.chrome_options = Options()
+        self.chrome_options.headless = True
         self.driver = webdriver.Chrome(options=self.chrome_options)
+        self.horse_links = set()
 
-    def navigate_pages(self, days):
+    def navigate_pages(self, days: int):
         links_by_date = self.create_date_links(days)
-        horse_links = set()
         for link in links_by_date:
             self.driver.get(link)
             time.sleep(3)
@@ -20,16 +23,18 @@ class Scraper:
                 print('No Event.')
                 continue
             card_races = self.get_card_races()
-            horse_links = horse_links.union(self.get_runners())
+            print(self.get_runner_table())
+            self.horse_links = self.horse_links.union(self.get_runner_links())
             for race_link in card_races:
                 self.driver.get(race_link)
                 print(f'Accessed {race_link}')
                 time.sleep(2)
-                horse_links = horse_links.union(self.get_runners())
-            print(len(horse_links))
-        return True
+                self.horse_links = self.horse_links.union(
+                    self.get_runner_links())
+                self.get_runner_table()
+        return
 
-    def date_list(self, days):
+    def date_list(self, days: int):
         today = datetime.datetime.today()
         return [today - datetime.timedelta(days=x) for x in range(days)]
 
@@ -54,7 +59,18 @@ class Scraper:
         links = [race.get_attribute('href') for race in races]
         return links
 
-    def get_runners(self):
+    def get_runner_table(self):
+        table = self.driver.find_elements_by_xpath(
+            '//table/tbody[@class="f_fs12"]/tr'
+        )
+        table = [self.get_runner(x) for x in table]
+        return np.array(table).T.tolist()
+
+    def get_runner(self, row):
+        runner = row.find_elements_by_xpath('.//td')
+        return [x.text for x in runner]
+
+    def get_runner_links(self):
         runners = self.driver.find_elements_by_xpath(
             '//table/tbody[@class="f_fs12"]/tr/td[3]/a'
         )
@@ -69,7 +85,7 @@ class Scraper:
 
 
 if __name__ == '__main__':
-    URL = ('https://racing.hkjc.com/racing/information/'
-           'English/Racing/LocalResults.aspx?RaceDate=2022/02/06')
-    scr = Scraper(URL)
-    scr.navigate_pages(3)
+    URL = ('https://racing.hkjc.com/racing/information/English/Racing'
+           '/LocalResults.aspx?RaceDate=2022/02/12&Racecourse=ST&RaceNo=2')
+    scr = Scraper()
+    scr.navigate_pages(4)
