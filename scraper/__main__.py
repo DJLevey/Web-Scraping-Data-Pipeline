343617efa004b04fe5356ca144c4c9c0b8cac0ec
Dax Levey <levey.dax@gmail.com>
2022-02-18 16:56:44 +0000

    sets working dir

--- scraper/__main__.py
+++ scraper/__main__.py
@@ -1,5 +1,7 @@
 from web_scraper import Scraper
+import os
 
 if __name__ == '__main__':
+    os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
     scr = Scraper()
-    scr.scrape_dates(scr.create_date_links(1))
+    scr.scrape_dates(scr.create_date_links(days=2))
