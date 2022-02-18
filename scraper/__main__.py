9e09df8b9db443693baef4d10d97fa9570ca341a
Dax Levey <levey.dax@gmail.com>
2022-02-18 17:13:00 +0000

    removes code

--- scraper/__main__.py
+++ scraper/__main__.py
@@ -1,7 +1,5 @@
 from web_scraper import Scraper
-import os
 
 if __name__ == '__main__':
-    os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
     scr = Scraper()
     scr.scrape_dates(scr.create_date_links(days=2))
