1e4c92934f17fb81d99a96942f082c80514bb78a
Dax Levey <levey.dax@gmail.com>
2022-02-21 17:15:43 +0000

    refactors __main__

--- scraper/__main__.py
+++ scraper/__main__.py
@@ -1,5 +1,7 @@
 from web_scraper import Scraper
 
 if __name__ == '__main__':
+    bucket = 'aicorebucket828'
+    num_dates = 3
     scr = Scraper()
-    scr.scrape_dates(scr.create_date_links(days=2))
+    scr.scrape_dates(scr.create_date_links(days=num_dates))
