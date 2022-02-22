47b13449a4cb17fc3c9693b615e297e25d79e5b9
Dax Levey <levey.dax@gmail.com>
2022-02-22 14:56:51 +0000

    adds method for uploading data folder to bucket

--- scraper/uploader.py
+++ scraper/uploader.py
@@ -1,22 +1,28 @@
 import boto3
 from botocore.exceptions import ClientError
-import logging
-import tempfile
-import shutil
 import os
 
 
-def upload_to_bucket(id: str, bucket):
+def upload_to_bucket_by_id(id: str, bucket='aicorebucket828') -> bool:
     s3_client = boto3.client('s3')
-    folder = f'../raw_data/{id}'
-    with tempfile.TemporaryDirectory() as tmp:
-        shutil.make_archive(os.path.join(tmp, id), 'zip', folder)
-        try:
-            s3_client.upload_file(
-                f'{os.path.join(tmp, id)}.zip',
-                bucket,
-                f'{id}.zip')
-        except ClientError as e:
-            logging.error(e)
-            return False
-        return True
+    folder = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), f'../raw_data/{id}')
+    for x in os.listdir(folder):
+        if x.startswith(id):
+            try:
+                s3_client.upload_file(
+                    os.path.join(folder, x),
+                    bucket,
+                    os.path.split(x)[1]
+                )
+            except ClientError as e:
+                print(e)
+                return False
+    return True
+
+
+def upload_folder_to_bucket(bucket='aicorebucket828'):
+    folder = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), '../raw_data/')
+    for x in os.listdir(folder):
+        upload_to_bucket_by_id(x)
