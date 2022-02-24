51759384c72a4e327f95d82a52815afabc8727d7
Dax Levey <levey.dax@gmail.com>
2022-02-24 14:48:21 +0000

    Adds methods to upload data

--- scraper/uploader.py
+++ scraper/uploader.py
@@ -1,5 +1,9 @@
 import boto3
 from botocore.exceptions import ClientError
+from sqlalchemy import Integer, create_engine, Table, Column, String, MetaData
+from sqlalchemy.engine.base import Connection
+import pandas as pd
+import json
 import os
 
 
@@ -26,3 +30,89 @@ def upload_folder_to_bucket(bucket='aicorebucket828'):
             os.path.dirname(os.path.abspath(__file__)), '../raw_data/')
     for x in os.listdir(folder):
         upload_to_bucket_by_id(x)
+
+
+def upload_to_rds_by_id(id: str, db) -> bool:
+    folder = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), f'../raw_data/{id}')
+    for x in os.listdir(folder):
+        if x.endswith('.json'):
+            with open(os.path.join(folder, x), 'r') as f:
+                data = json.load(f)
+                _race_insert(
+                    {i: data[i] for i in data if i != 'runners'},
+                    db
+                    )
+                engine = _connect_to_rds(db)
+                _runner_insert(engine, data.pop('runners'), db)
+    return True
+
+
+def create_table(db) -> bool:
+    meta = MetaData()
+    meta = _create_race_table(meta)
+    meta.create_all(_connect_to_rds(db))
+    return True
+
+
+def _race_insert(engine, race, db) -> bool:
+    engine = _connect_to_rds(db)
+    race = {x: [y] for x, y in race.items()}
+    race_dataframe = pd.DataFrame.from_dict(race)
+    race_dataframe.to_sql('race', engine, if_exists='replace')
+    return engine
+
+
+def _runner_insert(engine, runners, db) -> bool:
+    return engine
+
+
+def _connect_to_rds(db) -> Connection:
+    engine = create_engine(
+        f"{db['DATABASE_TYPE']}+{db['DBAPI']}://{db['USER']}"
+        f":{db['PASSWORD']}@{db['ENDPOINT']}:{db['PORT']}"
+        f"/{db['DATABASE']}"
+        )
+    print(engine.table_names())
+    return engine.connect()
+
+
+def _create_race_table(meta) -> MetaData:
+    Table(
+        'race', meta,
+        Column('uuid', String, primary_key=True, nullable=False),
+        Column('id', String, unique=True, nullable=False),
+        Column('date', String, nullable=False),
+        Column('race_number', Integer, nullable=False),
+        Column('class', String, nullable=False),
+        Column('length', Integer, nullable=False),
+        Column('going', String, nullable=False),
+        Column('course', String, nullable=False),
+        Column('prize', String, nullable=False),
+        Column('pace', String, nullable=False),
+        Column('url', String, nullable=False),
+        Column('image_link', String, nullable=False)
+    )
+    return meta
+
+
+def _create_runner_table(meta) -> MetaData:
+    Table(
+        'runner', meta,
+        Column('uuid', String, primary_key=True),
+        Column('race_id', String, nullable=False),
+        Column('place', Integer, nullable=False),
+        Column('number', Integer, nullable=False),
+        Column('name', String, nullable=False),
+        Column('jockey', String, nullable=False),
+        Column('trainer', String, nullable=False),
+        Column('actual weight', String, nullable=False),
+        Column('declared_weight', String, nullable=False),
+        Column('draw', Integer, nullable=False),
+        Column('length_behind_winner', String, nullable=False),
+        Column('running_positions', String, nullable=False),
+        Column('finish_time', String, nullable=False),
+        Column('win_odds', String, nullable=False),
+        Column('url', String, nullable=False)
+    )
+    return meta
