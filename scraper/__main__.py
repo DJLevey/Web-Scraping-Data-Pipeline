49a6c962a043836c11ef58707fe2152fbbf74606
Dax Levey <levey.dax@gmail.com>
2022-02-24 14:48:44 +0000

    adds config parser

--- scraper/__main__.py
+++ scraper/__main__.py
@@ -1,7 +1,15 @@
 from web_scraper import Scraper
+# import uploader
+import configparser
+import os
+
 
 if __name__ == '__main__':
-    bucket = 'aicorebucket828'
+    config = configparser.ConfigParser()
+    f = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), '../config.ini')
+    config.read(f)
+#     uploader.upload_to_rds_by_id('20-02-2022-1', config['RDS'])
     num_dates = 3
     scr = Scraper()
     scr.scrape_dates(scr.create_date_links(days=num_dates))
