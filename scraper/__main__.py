6c259e255d08738312aeb2558a5f43cb36424734
Dax Levey <levey.dax@gmail.com>
2022-02-28 14:19:31 +0000

    adds launch params

--- scraper/__main__.py
+++ scraper/__main__.py
@@ -1,15 +1,16 @@
 from web_scraper import Scraper
-# import uploader
 import configparser
 import os
-
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     f = os.path.join(
             os.path.dirname(os.path.abspath(__file__)), '../config.ini')
     config.read(f)
-#     uploader.upload_to_rds_by_id('20-02-2022-1', config['RDS'])
     num_dates = 3
     scr = Scraper()
-    scr.scrape_dates(scr.create_date_links(days=num_dates))
+    scr.scrape_dates(
+            scr.create_date_links(days=num_dates),
+            config['RDS'],
+            config['S3']['bucket']
+            )
