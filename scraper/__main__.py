5fbfbd05ecf9e522b5dda6460dc4c268351da340
Dax Levey <levey.dax@gmail.com>
2022-03-16 19:10:30 +0000

    changes num_dates to env var

--- scraper/__main__.py
+++ scraper/__main__.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     f = os.path.join(
             os.path.dirname(os.path.abspath(__file__)), '../config.ini')
     config.read(f)
-    num_dates = 1000
+    num_dates = os.getenv('HISTORIC_DATES', default=1)
     scr = Scraper()
     scr.scrape_dates(
             scr.create_date_links(days=num_dates),
