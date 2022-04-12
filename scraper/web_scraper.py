0adb9a227c91e59da2e127b5fa9cba4d6709b185
Dax Levey <levey.dax@gmail.com>
2022-04-12 12:21:32 +0100

    fixes bad url error exception

--- scraper/web_scraper.py
+++ scraper/web_scraper.py
@@ -84,12 +84,14 @@ class Scraper(object):
             DBAPI: Database API (psycopg2)
         '''
         try:
+            print(links)
             base_url = ('https://racing.hkjc.com/racing/information/'
                         'English/Racing/LocalResults.aspx?RaceDate=')
             if not all([url.startswith(base_url) for url in links]):
                 raise ValueError('Invalid URL found.')
         except ValueError as e:
             print(e)
+            raise
         retrieved_urls = get_retrieved_urls(db)
         no_event_urls = get_no_event_urls(db)
         for link in links:
